6acce87ff6e89ecf959a8ae9270fcbce4922f271
sfozturk61 <sfozturk61@gmail.com>
2020-04-12 20:14:14 -0400

    Updated data generation
    - CCD pixel is divided into 5,
    - actual_lattice showing where the atoms actual are is added as an output

--- lattice_generation.py
+++ lattice_generation.py
@@ -41,6 +41,13 @@ class GeneratedLatticeImage():
         x_index = atom_location_index[0,:] #atoms x location
         y_index = atom_location_index[1,:] #atoms y location
         
+        #Store actual occupation of the atoms for future comparison with the inferred one
+        lims = np.arange(0, (N+1)*M, M) - (N*M)/2
+        actual_lattice = np.zeros((N, N));
+        for ny in range(N):
+            for nx in range(N):
+                actual_lattice[ny, nx] = np.sum(np.where((x_index > lims[nx]) & (x_index < lims[nx+1]) & (y_index > lims[-(ny+2)]) & (y_index < lims[-(ny+1)]), 1, 0))
+            
         #For each atom sample photons from a Gaussian centered on the lattice site, combine the x,y positions of the counts
         x_loc = np.array([])
         y_loc = np.array([])
@@ -50,18 +57,18 @@ class GeneratedLatticeImage():
             y_loc = np.concatenate((y_loc, yy)) #combine the sampled y-locations for each atom
             
         #Generate dark counts which is the background noise of the camera. Combine dark photon locations with scattered photon locations.
-        CCD_x = np.arange(0, N*M, 1) - ((N*M)/2) #x-pixel locations
-        CCD_y = np.arange(0, N*M, 1) - ((N*M)/2) #y-pixel locations
+        CCD_x = np.arange(0, N*M, 0.2) - ((N*M)/2) #x-pixel locations
+        CCD_y = np.arange(0, N*M, 0.2) - ((N*M)/2) #y-pixel locations
         dark_count = np.random.poisson(lam_backg, N_backg) #create dark counts sampling from a Poisson distribution, this gives numbers corresponding to number of dark counts
         dark_count_location_x = np.random.choice(CCD_x, np.sum(dark_count), replace=True) #pick a random x location for the dark counts
         dark_count_location_y = np.random.choice(CCD_y, np.sum(dark_count), replace=True) #pick a random y location for the dark counts
         x_loc = np.concatenate((x_loc, dark_count_location_x)) #combine the sampled x-locations from atoms and dark counts
         y_loc = np.concatenate((y_loc, dark_count_location_y)) #combine the sampled y-locations from atoms and dark counts
         
-        
         # Store output results
         self.x_loc = x_loc
         self.y_loc = y_loc
+        self.actual_lattice = actual_lattice
 
     def plot(self):
         '''Plot the image (collected photons) on the camera.'''

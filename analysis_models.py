94b4d25ac3be87446701667fd6976ce509e4be10
sfozturk61 <sfozturk61@gmail.com>
2020-04-13 20:46:14 -0400

    ..

---
+++ analysis_models.py
@@ -0,0 +1,50 @@
+import numpy as np
+import pymc3 as pm
+import theano.tensor as tt
+
+class AnalysisModels():
+
+    ''' Class analyzing generated images with different models.'''
+
+    def __init__(self, x, y, std, xsite, ysite):
+        ''' Initialize empty object
+
+        Parameters
+        ----------
+
+        '''
+        self.x = x
+        self.y = y
+        self.std = std
+        self.xsite = xsite
+        self.ysite = ysite
+        
+    def mixture_model(x, y, std, xsite, ysite):
+
+        with pm.Model() as mixture_model_v0:
+
+            #Prior
+            P = pm.Uniform('P', lower=0, upper=1)
+
+            xc = (xsite[0]+xsite[1])/2 #x center of the site
+            yc = (ysite[0]+ysite[1])/2 #y center of the site
+
+            #Photons scattered from the atoms are Gaussian distributed
+            atom_x = pm.Normal.dist(mu=xc, sigma=std).logp(x)
+            atom_y = pm.Normal.dist(mu=yc, sigma=std).logp(y)
+            atom = atom_x + atom_y
+
+            #Photons from the camera background are uniform distributed
+            background_x = pm.Uniform.dist(lower = xsite[0], upper = xsite[1]).logp(x)
+            background_y = pm.Uniform.dist(lower = ysite[0], upper = ysite[1]).logp(y)
+            background = background_x + background_y
+
+            #Log-likelihood
+            log_like = tt.log((P * tt.exp(atom) + (1-P) * tt.exp(background)))
+
+            pm.Potential('logp', log_like.sum())
+
+        map_estimate = pm.find_MAP(model=mixture_model_v0)
+        P_value = map_estimate["P"][0]
+        
+        return P_value
--- lattice_analysis.py
+++ lattice_analysis.py
@@ -6,67 +6,28 @@ import theano.tensor as tt
 
 class AnalyzeLatticeImages():
 
-    ''' Class containing all image information of an EMCCD acquired image of a atom lattice'''
+    ''' Class analyzing generated images with different models.'''
 
     def __init__(self, N, M, std, x_loc, y_loc):
-        ''' Initialize empty LatticeImage object
+        ''' Initialize empty object
 
         Parameters
         ----------
-        name : A reference for this image.
-        N : Dimension of optical lattice.
-        M : Number of pixels per one side of lattice side.
-        jpeg_path: The path of the image file to be loaded.
+
         '''
         
         # Store dimensions as member variables.
         self.N = N
         self.M = M 
-        self.std = std   
-        
-        # Store output results
-        self.x_loc = x_loc
-        self.y_loc = y_loc
-
-    def mixture_model(self, x, y, std, xsite, ysite):
-
-        with pm.Model() as test_model:
-
-            #Prior
-            P = pm.Uniform('P', lower=0, upper=1)
-
-            xc = (xsite[0]+xsite[1])/2 #x center of the site
-            yc = (ysite[0]+ysite[1])/2 #y center of the site
-
-            #Photons scattered from the atoms are Gaussian distributed
-            atom_x = pm.Normal.dist(mu=xc, sigma=std).logp(x)
-            atom_y = pm.Normal.dist(mu=yc, sigma=std).logp(y)
-            atom = atom_x + atom_y
-
-            #Photons from the camera background are uniform distributed
-            background_x = pm.Uniform.dist(lower = xsite[0], upper = xsite[1]).logp(x)
-            background_y = pm.Uniform.dist(lower = ysite[0], upper = ysite[1]).logp(y)
-            background = background_x + background_y
-
-            #Log-likelihood
-            log_like = tt.log((P * tt.exp(atom) + (1-P) * tt.exp(background)))
-
-            pm.Potential('logp', log_like.sum())
-
-        map_estimate = pm.find_MAP(model=test_model)
-        P_value = map_estimate["P"][0]
-        
-        self.P_value = P_value
-        
-    def run_mixture_model(self, x_loc, y_loc, N, M, std):
+        self.std = std
         
         P_array = np.zeros((N,N))
         lims = np.arange(0, (N+1)*M, M) - (N*M)/2
         for ny in range(N):
             for nx in range(N):
-                x = np.where((x_loc > lims[nx]) & (x_loc <= lims[nx+1]) & (y_loc > lims[-(ny+2)]) & (y_loc <= lims[-(ny+1)]), x_loc, np.pi)
+                x = np.where((self.x_loc > lims[nx]) & (self.x_loc <= lims[nx+1]) & (self.y_loc > lims[-(ny+2)]) & (self.y_loc <= lims[-(ny+1)]), self.x_loc, np.pi)
                 x_new = x[x != np.pi]
-                y = np.where((x_loc > lims[nx]) & (x_loc <= lims[nx+1]) & (y_loc > lims[-(ny+2)]) & (y_loc <= lims[-(ny+1)]), y_loc, np.pi)
+                y = np.where((self.x_loc > lims[nx]) & (self.x_loc <= lims[nx+1]) & (self.y_loc > lims[-(ny+2)]) & (self.y_loc <= lims[-(ny+1)]), self.y_loc, np.pi)
                 y_new = y[y != np.pi]
 
                 xsite = np.array([lims[nx], lims[nx+1]])
@@ -75,3 +36,4 @@ class AnalyzeLatticeImages():
                 P_array[ny,nx] = mixture_model(x_new, y_new, std, xsite, ysite)
                 
         self.P_array = P_array
+        
